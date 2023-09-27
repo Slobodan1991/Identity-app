399c8f6a43de2ebc619380037e708f9e0fc066df
Slobodan <sloba_7@hotmail.com>
2023-09-27 20:56:52 +0200

    Created skills js file and update experience page.

--- src/views/ExperiencePage.vue
+++ src/views/ExperiencePage.vue
@@ -8,12 +8,51 @@
                     <p>What skills i have, and what i working on.</p>
                 </div>
             </div>
+            <div class="flex gap-x-5">
+                <div class="mt-6 flex w-1/2">
+                    <div
+                        class="text-base font-poppins w-full h-fit gap-x-5 flex flex-col gap-y-2 border py-5 px-6 shadow-lg text-center flex-wrap rounded-lg bg-gray-100">
+                        <h1 class="mb-5 font-poppins gradient-text font-semibold text-xl">Frontend skills</h1>
+                        <div class="w-full flex flex-wrap justify-center">
+                            <div v-for="(skill, index) in frontendSkills" :key="index"
+                                class="flex items-center flex-col mb-5 gap-y-1 p-3 w-[25%]">
+                                <div class="icon-size" v-html="skill.icon"></div>
+                                <p class="gradient-text font-semibold font-poppins text-lg my-1">{{ skill.name }}</p>
+                                <p class="text-sm font-normal font-poppins text-gray-500">{{ skill.level }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-6 flex w-1/2">
+                    <div
+                        class="text-base font-poppins w-full h-fit gap-x-5 flex flex-col gap-y-2 border py-5 px-6 shadow-lg text-center flex-wrap rounded-lg bg-gray-100">
+                        <h1 class="mb-5 font-poppins gradient-text font-semibold text-xl">Other skills</h1>
+                        <div class="w-full flex flex-wrap justify-center">
+                            <div v-for="(skill, index) in otherSkills" :key="index"
+                                class="flex items-center flex-col mb-5 gap-y-1 p-3 w-[25%]">
+                                <div v-html="skill.icon"></div>
+                                <p class="gradient-text font-semibold font-poppins text-lg my-1">{{ skill.name }}</p>
+                                <p class="text-sm font-normal font-poppins text-gray-500">{{ skill.level }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
 <script>
+import { frontendSkills, otherSkills } from '../contstants/skills'
+
 export default {
-    name: 'ExperiencePage'
+    name: 'ExperiencePage',
+    data() {
+        return {
+            frontendSkills,
+            otherSkills
+        };
+    },
 }
 </script>
 <style scoped></style>
